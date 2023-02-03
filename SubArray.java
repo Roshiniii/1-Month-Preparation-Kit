d196e408e12c333d274fb5aeef6d3e611ed3c679
Roshiniii <94703050+Roshiniii@users.noreply.github.com>
2023-02-03 11:41:28 +0530

    Create SubArray.java

---
+++ SubArray.java
@@ -0,0 +1,19 @@
+public static int birthday(List<Integer> s, int d, int m) {
+    // Write your code here
+    int count = 0;
+    int n = s.size();
+    for(int i = 0;i<n-(m-1);i++){
+        int sum = 0;
+        for(int j = i ; j<m+i;j++)
+            {
+                sum+=s.get(j);
+            
+            }
+        if(sum == d){
+                count++;
+            }
+        
+    }
+    return count;
+
+    }
